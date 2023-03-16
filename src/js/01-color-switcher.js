18011ec9ef778dcd9e6a4123e16150cf6ffda5f3
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-16 20:26:36 +0100

    222

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timeId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+startButton.addEventListener('click', () => {
+  timeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+stopButton.addEventListener('click', () => {
+  clearInterval(timeId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
