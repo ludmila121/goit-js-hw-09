cdeb59e5865c44e9c0f33ffc83089ebfd4508162
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-18 21:42:50 +0100

    222

---
+++ src/js/02-timer.js
@@ -0,0 +1,28 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+let selectedTime = null;
+
+const refs = {
+    inputData: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('button [data-start]'),
+    days: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    minutes: document.querySelector('span[data-minutes]'),
+    seconds: document.querySelector('span[data-seconds]'),
+};
+ 
+function convertMs(ms){
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
